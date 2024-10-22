37cf3650625b53f8fc182ce2d037965d50101a67
KAndrade-SP <kaique.viana090@gmail.com>
2024-10-22 14:33:34 -0300

    fix: fix useEffect googlesignIn, creating a new app in firebase with new credentials and hiding them in gitignore

--- App.tsx
+++ App.tsx
@@ -5,8 +5,12 @@ import { NavigationContainer } from '@react-navigation/native'
 import AppNavigator from './src/navigation/AppNavigator'
 import { Provider } from 'react-redux'
 import { store } from './src/redux/store'
+import { useFirebaseConfig } from './src/services/firebase'
 
 function App(): React.JSX.Element {
+
+  useFirebaseConfig()
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,9 +1,18 @@
 import auth from '@react-native-firebase/auth'
 import { GoogleSignin } from '@react-native-google-signin/google-signin'
+import { useEffect } from 'react'
 import Config from 'react-native-config'
 
-GoogleSignin.configure({
-  webClientId: Config.FIREBASE_WEB_CLIENT_ID,
-})
+const initializeGoogleSignin = () => {
+  GoogleSignin.configure({
+    webClientId: Config.FIREBASE_WEB_CLIENT_ID,
+  })
+}
 
 export const firebaseAuth = auth()
+
+export const useFirebaseConfig = () => {
+  useEffect(() => {
+    initializeGoogleSignin()
+  }, [])
+}
